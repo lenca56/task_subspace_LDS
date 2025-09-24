c744965d64fd852072542f418bee958415469d7a
lc3919 <lc3919@axon.rc.zi.columbia.edu>
2025-09-24 18:27:42 -0400

    checked EM recovers true parameters and elbo,ll work as expected

--- plotting_utils.py
+++ plotting_utils.py
@@ -9,12 +9,12 @@ import jax.numpy as jnp
 #   b = np.random.uniform(0, 1, 1)[0]
 #   return (r, g, b)
 
-# def plot_mse_parameters(axes, K1, A, B, Q, mu0, Q0, C, d, R, true_A, true_B, true_Q, true_mu0, true_Q0, true_C, true_d, true_R):
-#     axes.set_ylabel('mse')
-#     # axes.set_ylim(0,0.001)
-#     axes.scatter(range(10), [mse(A[:K1,:K1], true_A[:K1,:K1]), mse(A[K1:,:K1], true_A[K1:,:K1]), mse(A[K1:,K1:], true_A[K1:,K1:]), mse(B[:K1], true_B[:K1]), mse(Q, true_Q), mse(mu0.reshape(mu0.shape[0],1), true_mu0.reshape(mu0.shape[0],1)), mse(Q0, true_Q0), mse(C, true_C), mse(d.reshape(d.shape[0],1), true_d.reshape(d.shape[0],1)), mse(R, true_R)])
-#     axes.set_xticks(range(10), ['A11', 'A21', 'A22', 'B','Q', 'mu0', 'Q0', 'C_', 'd', 'R'])
-
+def plot_mse_parameters(axes, K1, A, B, Q, mu0, Q0, C, d, R, true_A, true_B, true_Q, true_mu0, true_Q0, true_C, true_d, true_R):
+    axes.set_ylabel('mse')
+    # axes.set_ylim(0,0.001)
+    axes.scatter(range(11), [mse(A[:K1,:K1], true_A[:K1,:K1]), mse(A[K1:,:K1], true_A[K1:,:K1]), mse(A[K1:,K1:], true_A[K1:,K1:]), mse(B[:K1], true_B[:K1]), mse(Q, true_Q), mse(mu0, true_mu0), mse(Q0, true_Q0), mse(C[:,:K1], true_C[:,:K1]), mse(C[:,K1:], true_C[:,K1:]), mse(d, true_d), mse(R, true_R)])
+    axes.set_xticks(range(11), ['A11', 'A21', 'A22', 'B1','Q', 'mu0', 'Q0', 'C1', 'C2', 'd', 'R'])
+ 
 def plot_eigenvalues(axes, eigval1, eigval2, color='black', label=['',''], alpha=1):
     axes.scatter(jnp.real(eigval1), jnp.imag(eigval1), color=color, label=label[0], marker='o', alpha=alpha)
     axes.scatter(jnp.real(eigval2), jnp.imag(eigval2), color=color, label=label[1], marker='s', alpha=alpha)
